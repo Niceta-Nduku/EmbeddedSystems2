5c048c569b914a64de37b7d26e7af0b03fbc2dbf
Niceta Nduku <nicetanduku@gmail.com>
2019-07-23 19:24:55 +0100

    buttons switch LEDs on and off with interrupt and debounce

--- Prac1.py
+++ Prac1.py
@@ -12,26 +12,31 @@ LED_List = (5,6,12) #LED channels
 on_button = 18 #on button
 off_button = 17 #off button
 
-def main():
-	if GPIO.input(on_button)== GPIO.HIGH:
-		print ("on button pressed")
-		GPIO.output(LED_List,GPIO.HIGH)
-		print ("LED on")
-	if GPIO.input(off_button)==GPIO.HIGH:
-		print("off button pressed")
-		GPIO.output(LED_List,GPIO.LOW)
-		print("LED off")
 def init_GPIO():
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setwarnings(False)
 	GPIO.setup([off_button,on_button],GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 	GPIO.setup(LED_List,GPIO.OUT, initial=GPIO.LOW)
 
-init_GPIO()
+	GPIO.add_event_detect(on_button, GPIO.RISING, callback=on_callback,bouncetime=300)
+	GPIO.add_event_detect(off_button, GPIO.RISING, callback=off_callback, bouncetime=300)
+
+def on_callback(channel):
+	GPIO.output(LED_List,GPIO.HIGH)
+	print ("LED on")
+
+def off_callback(channel):
+	GPIO.output(LED_List,GPIO.LOW)
+	print ("LED off")
+
+def main():
+	time.sleep(3)
 
 # Only run the functions if 
 if __name__ == "__main__":
     # Make sure the GPIO is stopped correctly
+    init_GPIO()
+
     try:
         while True:
             main()
