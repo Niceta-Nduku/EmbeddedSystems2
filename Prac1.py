2803ba038c957d93c8a768ab28aa1c4964265ca7
Niceta Nduku <nicetanduku@gmail.com>
2019-07-23 21:56:47 +0100

    push buttons and LEDs set to count

--- Prac1.py
+++ Prac1.py
@@ -8,29 +8,50 @@ Date: 23/07/2019
 
 import RPi.GPIO as GPIO
 import time
-LED_List = (5,6,12) #LED channels
-on_button = 18 #on button
-off_button = 17 #off button
+from itertools import product
+
+lsb=5 #least significant bit
+msb =  12 #most significant bit
+sec_bit =  6 #middle digit
+
+up_button = 18 #button to count down
+down_button = 17 #button to count down
+binary_digits = list(product(range(2),repeat=3)) #binary values from 000 to 1
+current = 0 #the iterator that will determine what the current nuber is
 
 def init_GPIO():
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setwarnings(False)
-	GPIO.setup([off_button,on_button],GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-	GPIO.setup(LED_List,GPIO.OUT, initial=GPIO.LOW)
+	GPIO.setup([down_button,up_button],GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+	GPIO.setup((msb,sec_bit,lsb),GPIO.OUT, initial= 0)
 
-	GPIO.add_event_detect(on_button, GPIO.RISING, callback=on_callback,bouncetime=300)
-	GPIO.add_event_detect(off_button, GPIO.RISING, callback=off_callback, bouncetime=300)
+	GPIO.add_event_detect(up_button, GPIO.RISING, callback=count_up,bouncetime=300)
+	GPIO.add_event_detect(down_button, GPIO.RISING, callback=count_down, bouncetime=300)
 
-def on_callback(channel):
-	GPIO.output(LED_List,GPIO.HIGH)
-	print ("LED on")
+def count_up(channel):
+	global current
+	print("Counting up")
+	while True:
+		if current==8:
+			current=0
+		time.sleep(1)
+		GPIO.output((msb,sec_bit,lsb),binary_digits[current])
+		print(binary_digits[current])
+		current+=1
 
-def off_callback(channel):
-	GPIO.output(LED_List,GPIO.LOW)
-	print ("LED off")
+def count_down(channel):
+	global current
+	print("Counting down")
+	while True:
+		if current==-1:
+			current=7
+		time.sleep(1)
+		GPIO.output((msb,sec_bit,lsb),binary_digits[current])
+		print(binary_digits[current])
+		current-=1
 
 def main():
-	time.sleep(3)
+	time.sleep(1)
 
 # Only run the functions if 
 if __name__ == "__main__":
