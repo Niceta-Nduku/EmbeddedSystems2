051384bde29f6ed78ffb5ade4a97e0dc3372f854
Niceta Nduku <nicetanduku@gmail.com>
2019-07-24 20:22:03 +0100

    Complete binary counter code

--- NDKNIC001_Prac1.py
+++ NDKNIC001_Prac1.py
@@ -17,41 +17,42 @@ sec_bit =  6 #middle digit
 up_button = 18 #button to count down
 down_button = 17 #button to count down
 binary_digits = list(product(range(2),repeat=3)) #binary values from 000 to 1
-current = 0 #the iterator that will determine what the current nuber is
+current = 0 #the iterator that will determine what the current number is
 
 def init_GPIO():
-	GPIO.setmode(GPIO.BCM)
+	GPIO.setmode(GPIO.BCM) #reading board as BCM
 	GPIO.setwarnings(False)
-	GPIO.setup([down_button,up_button],GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+	#up and down buttons set as pull down
+	GPIO.setup([down_button,up_button],GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
+
+	#Leds all set to low
 	GPIO.setup((msb,sec_bit,lsb),GPIO.OUT, initial= 0)
 
+	#event detectors for up and down buttons
 	GPIO.add_event_detect(up_button, GPIO.RISING, callback=count_up,bouncetime=300)
 	GPIO.add_event_detect(down_button, GPIO.RISING, callback=count_down, bouncetime=300)
 
 def count_up(channel):
 	global current
-	print("Counting up")
-	while True:
-		if current==8:
-			current=0
-		time.sleep(1)
-		GPIO.output((msb,sec_bit,lsb),binary_digits[current])
-		print(binary_digits[current])
-		current+=1
+	current+=1 #increment
+	if current== 8:
+		#wrap
+		current=0
+	GPIO.output((msb,sec_bit,lsb),binary_digits[current])
+	print(binary_digits[current]) #display in terminal
 
 def count_down(channel):
 	global current
-	print("Counting down")
-	while True:
-		if current==-1:
-			current=7
-		time.sleep(1)
-		GPIO.output((msb,sec_bit,lsb),binary_digits[current])
-		print(binary_digits[current])
-		current-=1
+	current-=1 #decrement
+	if current==-1: 
+		#wrap 
+		current=7
+	GPIO.output((msb,sec_bit,lsb),binary_digits[current])
+	print(binary_digits[current])
 
 def main():
-	time.sleep(1)
+	time.sleep(1) #do nothing 
 
 # Only run the functions if 
 if __name__ == "__main__":
@@ -63,7 +64,7 @@ if __name__ == "__main__":
             main()
     except KeyboardInterrupt:
         print("Exiting gracefully")
-        # Turn off your GPIOs here
+        # Turn off your GPIOs
         GPIO.cleanup()
     except e:
         GPIO.cleanup()
